d3d6522066bbcff7bfaa4d5d718dc98598fb9d48
M-JM <jimmymiels@hotmail.com>
2021-06-02 17:26:52 +0200

    Initial commit

---
+++ src/main/java/be/OdiMentee/GatewayService/config/GatewayServiceConfig.java
@@ -0,0 +1,41 @@
+package be.OdiMentee.GatewayService.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableHystrix
+public class GatewayServiceConfig {
+
+    @Autowired
+    AuthenticationFilter filter;
+
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("profielen-service", r -> r.path("/profielen/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://PROFIELEN-SERVICE"))
+
+                .route("domein-service", r -> r.path("/domein/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://DOMEIN-SERVICE"))
+
+                .route("auth-service", r -> r.path("/users/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://AUTH-SERVICE"))
+                .build();
+    }
+
+
+
+    }
+
+
+
---
+++ src/main/java/be/OdiMentee/GatewayService/config/RouterValidator.java
@@ -0,0 +1,26 @@
+package be.OdiMentee.GatewayService.config;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouterValidator {
+
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/users/generate-token",
+            "/users/signup",
+            "/users/test",
+            "/domein/test",
+            "/profielen/Studenten"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
